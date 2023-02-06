ca0c644a0abf4ae9ddcecce34d12e5021737bdc6
Prasetia Cahya Hadi <cahya.cahya.pc@gmail.com>
2023-02-06 12:29:40 +0700

    new file:   step02.vue

---
+++ step02.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  // component options
+  // declare some reactive state here.
+  data(){
+    return {
+      message : 'Hello world !'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{message}}</h1>
+</template>
