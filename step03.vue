5f0dedc060748e63a271d632aa1f92819387c954
Prasetia Cahya Hadi <cahya.cahya.pc@gmail.com>
2023-02-06 13:01:25 +0700

    new file:   step03.vue

---
+++ step03.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return {
+      titleClass: 'title',
+      pClass: 'p-class'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1 :class="titleClass">Make me red</h1>
+  <p :class="pClass">jadikan aku biru</p>
+</template>
+
+<style>
+.title {
+  color: red;
+}
+  .p-class{
+    color: #003cff;
+  }
+</style>
